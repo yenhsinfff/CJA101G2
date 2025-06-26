960212776da6b08b14a199a05d7696dac22b892f
Ollie Shih <shih467864@gmail.com>
2025-06-26 10:41:09 +0800

    [Feat] 新增購物車修改、移除、清空、合併功能

--- src/main/java/com/lutu/cart/controller/CartController.java
+++ src/main/java/com/lutu/cart/controller/CartController.java
@@ -40,7 +40,6 @@ public class CartController {
 
 	}
 
-	
 	// 查詢購物車內容
 	@GetMapping("/api/getCart")
 	public ApiResponse<List<CartDTO_res>> getCart(HttpSession session, @RequestParam(required = false) Integer memId) {
@@ -51,6 +50,62 @@ public class CartController {
 			return new ApiResponse<>("success", cartList, "查詢成功");
 		} catch (Exception e) {
 			return new ApiResponse<>("fail", null, "查詢失敗");
+		}
+
+	}
+
+	// 修改購物車
+	@PostMapping("/api/updateCart")
+	public ApiResponse<CartDTO_res> updateCart(HttpSession session, @RequestParam(required = false) Integer memId,
+			@RequestParam Integer prodId, @RequestParam Integer prodColorId, @RequestParam Integer prodSpecId,
+			@RequestParam Integer cartProdQty) {
+		try {
+			CartDTO_res result = cs.updateCart(session, memId, prodId, prodColorId, prodSpecId, cartProdQty);
+
+			return new ApiResponse<CartDTO_res>("success", result, "修改成功");
+		} catch (Exception e) {
+			return new ApiResponse<CartDTO_res>("fail", null, "修改失敗");
+		}
+
+	}
+
+	// 移除購物車細項
+	@PostMapping("/api/removeCart")
+	public ApiResponse<List<CartDTO_res>> removeCart(HttpSession session, @RequestParam(required = false) Integer memId,
+			@RequestParam Integer prodId, @RequestParam Integer prodColorId, @RequestParam Integer prodSpecId) {
+		try {
+			List<CartDTO_res> newList = cs.removeCart(session, memId, prodId, prodColorId, prodSpecId);
+
+			return new ApiResponse<>("success", newList, "移除成功");
+		} catch (Exception e) {
+			return new ApiResponse<>("fail", null, "移除失敗");
+		}
+
+	}
+
+	// 清空物車
+	@PostMapping("/api/clearCart")
+	public ApiResponse<List<CartDTO_res>> clearCart(HttpSession session, @RequestParam(required = false) Integer memId) {
+		try {
+			List<CartDTO_res> clearList = cs.clearCart(session, memId);
+
+			return new ApiResponse<>("success", clearList, "清空成功");
+		} catch (Exception e) {
+			return new ApiResponse<>("fail", null, "清空失敗");
+		}
+
+	}
+
+	// 合併購物車
+	@PostMapping("/api/mergeCart")
+	public ApiResponse<List<CartDTO_res>> mergeCart(HttpSession session, Integer memId) {
+
+		try {
+			List<CartDTO_res> mergeCart = cs.mergeCart(session, memId);
+
+			return new ApiResponse<>("success", mergeCart, "合併成功");
+		} catch (Exception e) {
+			return new ApiResponse<>("fail", null, "合併失敗");
 		}
 
 	}
--- src/main/java/com/lutu/shop_order_items_details/model/ShopOrderItemsDetailsDTO_res.java
+++ src/main/java/com/lutu/shop_order_items_details/model/ShopOrderItemsDetailsDTO_res.java
@@ -10,7 +10,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
-public class ShopOrderItemsDetailsDTO_insert_res {
+public class ShopOrderItemsDetailsDTO_res {
+	
+	@NotNull(message = "請輸入訂單編號")
+	private Integer shopOrderId; // 訂單編號
 
 	@NotNull(message = "請輸入商品編號")
 	private Integer prodId; // 商品編號
@@ -21,23 +24,34 @@ public class ShopOrderItemsDetailsDTO_insert_res {
 	private Integer shopOrderQty; // 購買數量
 
 	// 商品訂單明細新增時不會填寫
-//	private Byte commentSatis; // 評價滿意度
-//
-//	private String commentContent; // 評價內容
-//
-//	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-//	private LocalDateTime commentDate; // 評價日期
+	private Byte commentSatis; // 評價滿意度
+
+	// 商品訂單明細新增時不會填寫
+	private String commentContent; // 評價內容
+
+	// 商品訂單明細新增時不會填寫
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+	private LocalDateTime commentDate; // 評價日期
 	
 	private String prodColorName; // 商品顏色名稱
 
 	private String prodSpecName; // 商品規格名稱
 
-	public ShopOrderItemsDetailsDTO_insert_res() {
+	public ShopOrderItemsDetailsDTO_res() {
 	}
 
 	// --- Getters and Setters ---
+	
 	public Integer getProdId() {
 		return prodId;
+	}
+
+	public Integer getShopOrderId() {
+		return shopOrderId;
+	}
+
+	public void setShopOrderId(Integer shopOrderId) {
+		this.shopOrderId = shopOrderId;
 	}
 
 	public void setProdId(Integer prodId) {
@@ -76,30 +90,30 @@ public class ShopOrderItemsDetailsDTO_insert_res {
 		this.prodSpecName = prodSpecName;
 	}
 	
-	//新增時不會填寫評分和評價
-//	public Byte getCommentSatis() {
-//		return commentSatis;
-//	}
-//
-//	public void setCommentSatis(Byte commentSatis) {
-//		this.commentSatis = commentSatis;
-//	}
-//
-//	public String getCommentContent() {
-//		return commentContent;
-//	}
-//
-//	public void setCommentContent(String commentContent) {
-//		this.commentContent = commentContent;
-//	}
-//
-//	public LocalDateTime getCommentDate() {
-//		return commentDate;
-//	}
-//
-//	public void setCommentDate(LocalDateTime commentDate) {
-//		this.commentDate = commentDate;
-//	}
+	// 新增時不會填寫評分和評價
+	public Byte getCommentSatis() {
+		return commentSatis;
+	}
+
+	public void setCommentSatis(Byte commentSatis) {
+		this.commentSatis = commentSatis;
+	}
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+
+	public LocalDateTime getCommentDate() {
+		return commentDate;
+	}
+
+	public void setCommentDate(LocalDateTime commentDate) {
+		this.commentDate = commentDate;
+	}
 
 	
 
