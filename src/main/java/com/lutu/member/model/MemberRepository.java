0b1f60ab1d4cffb9217d66a69703539c4d3a43b2
soccerchou <pingfeng24@gmail.com>
2025-07-01 10:10:35 +0800

    [FIX] 修改會員登入API

--- src/main/java/com/lutu/member/model/MemberRepository.java
+++ src/main/java/com/lutu/member/model/MemberRepository.java
@@ -4,6 +4,11 @@ package com.lutu.member.model;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.lutu.member.dto.MemberLoginDTO;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface MemberRepository extends JpaRepository<MemberVO, Integer> {
 	
